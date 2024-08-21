10f9a487455aac9bb49fc24188539b661475a851
FabioPinho <fabiopinhos@gmail.com>
2024-08-21 09:20:49 -0300

    feat: creating controller

---
+++ src/main/java/br/com/luizalabs/verticallogistica/controller/FileUploadController.java
@@ -0,0 +1,26 @@
+package br.com.luizalabs.verticallogistica.controller;
+
+import br.com.luizalabs.verticallogistica.application.impl.FileUploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/v1/api/files")
+public class FileUploadController {
+
+    @Autowired
+    private FileUploadService fileUploadService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> upload(@RequestParam("file")MultipartFile file) throws IOException {
+        fileUploadService.readFile(file);
+        return ResponseEntity.ok("OK");
+    }
+}
