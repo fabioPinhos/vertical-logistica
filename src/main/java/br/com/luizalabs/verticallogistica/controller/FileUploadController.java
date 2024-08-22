a053976dd59c08a82c26eb6d003c5115b9550142
FabioPinho <fabiopinhos@gmail.com>
2024-08-22 10:15:10 -0300

    feat: editing controllers, remove @Autowired

--- src/main/java/br/com/luizalabs/verticallogistica/controller/FileUploadController.java
+++ src/main/java/br/com/luizalabs/verticallogistica/controller/FileUploadController.java
@@ -1,7 +1,7 @@
 package br.com.luizalabs.verticallogistica.controller;
 
-import br.com.luizalabs.verticallogistica.application.IFileUploadService;
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.luizalabs.verticallogistica.application.service.IFileUploadService;
+import br.com.luizalabs.verticallogistica.core.domain.dto.UserDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,17 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/api/files")
 public class FileUploadController {
 
-    @Autowired
     private IFileUploadService fileUploadService;
 
+    public FileUploadController(IFileUploadService fileUploadService) {
+        this.fileUploadService = fileUploadService;
+    }
+
     @PostMapping("/upload")
-    public ResponseEntity<String> upload(@RequestParam("file")MultipartFile file) throws IOException {
-        fileUploadService.readFile(file);
-        return ResponseEntity.ok("OK");
+    public ResponseEntity<List<UserDto>> upload(@RequestParam("file")MultipartFile file) throws IOException {
+        List<UserDto> userDtos = fileUploadService.readFile(file);
+        return ResponseEntity.ok(userDtos);
     }
 }
